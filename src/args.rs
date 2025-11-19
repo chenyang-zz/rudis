7defa2f1c8fbfbedf621e22a51e4eebabf731617
赵晨炀 <904852749@qq.com>
2025-11-19 22:01:52 +0800

    feat: 添加args参数

---
+++ src/args.rs
@@ -0,0 +1,89 @@
+use clap::Parser;
+use std::str::FromStr;
+
+#[derive(Parser)]
+#[command(version, author, about, long_about = None)]
+pub struct Args {
+    /// 配置文件路径
+    #[arg(short, long, default_value = "rudis.conf")]
+    pub config: String,
+
+    /// 认证密码
+    #[arg(long)]
+    pub requirepass: Option<String>,
+
+    /// 绑定地址
+    #[arg(short, long, default_value = "0.0.0.0")]
+    pub bind: String,
+
+    /// 文件路径
+    #[arg(default_value = "data/dump.rdb")]
+    pub dbfilename: String,
+
+    /// 安装路径
+    #[arg(default_value = "./")]
+    pub dir: String,
+
+    /// 保存策略
+    #[clap(long, value_parser, value_delimiter = ' ', num_args = 1..)]
+    pub save: Vec<SaveRule>,
+
+    /// 数据库
+    #[arg(short, long, default_value = "16")]
+    pub databases: usize,
+
+    /// 监听频率
+    #[arg(long, default_value = "10.0")]
+    pub hz: f64,
+
+    /// 监听端口
+    #[arg(short, long, default_value = "6379")]
+    pub port: String,
+
+    /// 当前节点类型指示（用于标识节点在 Redis 集群架构中的角色）
+    /// 若为 None，则表明此节点为主节点；反之，若存在值，则表示该节点是从节点，其值对应所从属的主节点的相关信息
+    #[arg(long)]
+    pub replicaof: Option<String>,
+
+    /// 日志级别
+    #[arg(short, long, default_value = "info")]
+    pub loglevel: String,
+
+    /// 持久化配置 - 是否开启
+    #[arg(long, default_value = "no")]
+    pub appendonly: String,
+
+    /// 持久化配置 - 数据文件名称
+    #[arg(long, default_value = "data/dump.aof")]
+    pub appendfilename: String,
+
+    /// 持久化配置 - 持久化方式
+    #[arg(long, default_value = "always")]
+    pub appendfsync: String,
+}
+
+#[derive(Debug, Clone)]
+pub struct SaveRule {
+    pub seconds: u64,
+    pub changes: u64,
+}
+
+impl FromStr for SaveRule {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<&str> = s.split(',').collect();
+        if parts.len() < 2 {
+            return Err("Invalid save rule format. Expected 'seconds,changes'".into());
+        }
+
+        Ok(SaveRule {
+            seconds: parts[0]
+                .parse()
+                .map_err(|e| format!("Invalid seconds: {}", e))?,
+            changes: parts[1]
+                .parse()
+                .map_err(|e| format!("Invalid changes: {}", e))?,
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[tokio::main]
 async fn main() {
+    env_logger::init();
     service_into();
 }
 
